2e8f2b2d893e66efda39adaaebb4a708b26d7dbe
Buford <cbuford@una.edu>
2025-02-11 16:44:45 -0600

    almost done maybe

--- app.js
+++ app.js
@@ -20,114 +20,66 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
-async function getData(){
-  await client.connect();
-  let collection = await client.db("guitar-app-database").collection("guitar-app-songs");
-  let results = await collection.find({}).toArray();
-    
-  console.log(results);
-  return results;
+const mongoCollection = client.db("candiceSobieProfile").collection("candiceSobieBlog");
+
+function initProfileData(){
+  mongoCollection.insertOne({
+    title: "Dev tools",
+    post: "skills",
+  });
+ 
 }
 
-//endpoint, middleware(s)
 
-app.get('/read', async function(req,res){
-  let getDataResults = await getData();
-  console.log(getDataResults);
-  res.render('songs',
-    {songData : getDataResults}
+app.get('/', async function(req,res){
+  let results = await mongoCollection.find({}).toArray();
+  res.render('profile',
+    {mongoResults : results}
   );
 
 })
-app.get('/insert', async (req,res)=> {
+app.post('/insert', async (req,res)=> {
 
-  //console.log('in /insert');
-  let newSong = req.query.myName;
-  console.log(newSong);
-  //connect to db,
-  await client.connect();
-  //point to the collection 
-  await client.db("guitar-app-database").collection("guitar-app-songs").insertOne({ song_name: newSong});
+  await mongoCollection.insertOne({
+    title: req.body.title,
+    post: req.body.post
+
+  });
   
   //insert into it
-  res.redirect('/read');
+  res.redirect('/');
 
 }); 
 
-app.post('/delete/:id', async (req,res)=>{
+app.post('/delete', async (req,res)=>{
 
-  console.log("req.parms.id: ", req.params.id)
-
-  client.connect; 
-  const collection = client.db("guitar-app-database").collection("guitar-app-songs");
-  let result = await collection.findOneAndDelete( 
-  {"_id": new ObjectId(req.params.id)})
-
-.then(result => {
-  console.log(result); 
-  res.redirect('/read');
+  console.log(req.body.deleteId); 
+    let result = await mongoCollection.findOneAndDelete( 
+    {
+      "_id": new ObjectId(req.body.deleteId)
+    }
+  ).then(result => {
+    console.log(result); 
+    res.redirect('/');
+  })
 })
-})
+
 app.post('/update', async (req,res)=>{
 
-  console.log("req.body: ", req.body)
-
-  client.connect; 
-  const collection = client.db("guitar-app-database").collection("guitar-app-songs");
-  let result = await collection.findOneAndUpdate( 
-  {"_id": new ObjectId(req.body.nameID)}, { $set: {"fname": req.body.inputUpdateName } }
-)
-.then(result => {
-  console.log(result); 
-  res.redirect('/read');
-})
-});  
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
-})
-
-app.get('/nodemon', function (req, res) {
-  res.send('look ma, no hands and no dollars I dont dance<br><a href="/">back home</a>')
-})
-app.get('/helloRender', function (req, res) {
-  res.send('Hello Express from Real World<br><a href="/">back home</a>')
-})
-
-app.get('/ejs', function (req, res) {
-  res.render('words',
-    {pageTitle: 'my cool ejs page'}
-  );
-})
-app.post('/saveMyName', (req, res) =>{
-  console.log('did we hit the new input?');
-  console.log(req.body);
-  
-
- // res.redirect('/ejs');
-  res.render('words',
-    {pageTitle: req.body.myName}
-  );
-})
-app.get('/saveMyNameGet', (req, res) =>{
-  console.log('did we hit the new input get?');
-  console.log('req.query:', req.query);
-  
-  res.redirect('/ejs');
-})
+  let result = await mongoCollection.findOneAndUpdate( 
+    {_id: ObjectId.createFromHexString(req.body.updateId)}, { 
+      $set: 
+        {
+          title : req.body.updateTitle, 
+          post : req.body.updatePost 
+        }
+       }
+    ).then(result => {
+    console.log(result); 
+    res.redirect('/');
+  })
+  });  
 
 app.listen(port, 
   ()=> console.log(
